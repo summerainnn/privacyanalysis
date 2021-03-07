e4854315b4639633734e7e75731fe59fc129a342
summerainnn <709492145@qq.com>
2021-03-07 19:02:24 +0800

    Initial commit

---
+++ src/main/java/summerain/PrivacyanalysisApplication.java
@@ -0,0 +1,13 @@
+package summerain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyanalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivacyanalysisApplication.class, args);
+    }
+
+}
